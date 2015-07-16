51ce109cfd6c478769170a67deaad3e11568fab6
Maxim Doronin <maksim-doronin93@mail.ru>
2015-07-16 16:25:32 +0600

    Update

--- api.instagram.class.php
+++ api.instagram.class.php
@@ -1,59 +1,85 @@
 <?php
-
 /**
  * Instagram
  * @author Maxim Doronin <maksimdoronin93@gmail.com>
  */
 class Instagram
 {
-	public $userId = '';
-	public $clientId = '';
+	public $userId;
+	public $clientId;
 
 	/**
-	 * @param $argument
+	 * @param $user
+	 * @param $clientId
 	 */
-	function __construct($userName, $clientId)
+	function __construct($user, $clientId)
 	{
 		if (isset($clientId) && !empty($clientId)) {
 			$this->clientId = $clientId;
 		}
-		if (isset($userName) && !empty($userName)) {
-			$this->setUserIdByName($userName);
+		if (isset($user) && !empty($user)) {
+			if (!is_int($user)) {
+				$data = $this->getSearch($user);
+				$user = $data->data[0]->id;
+			}
+			$this->userId = $user;
 		}
 	}
 
-	private function setUserIdByName($userName)
-	{
-		$url = "https://api.instagram.com/v1/users/search";
-		$url .= "?q=" . $userName;
-		$url .= "&client_id=" . $this->clientId;
-		$data = json_decode($this->cUrl($url));
-		return $this->userId = $data->data[0]->id;
-	}
-
 	/**
+	 * Get basic information about a user. To get information about the owner of the access token, you can use self instead of the user-id.
 	 * @return Array
 	 */
-	public function getMedia()
+	public function getUser()
 	{
-		$url = "https://api.instagram.com/v1/users/{$this->userId}/media/recent/?client_id=" . $this->clientId;
+		$url = "https://api.instagram.com/v1/users/{$this->userId}/";
+		$url .= "?client_id=" . $this->clientId;
 		$data = json_decode($this->cUrl($url));
-		return $data->data;
+		return $this->cUrl($url);
 	}
 
 	/**
-	 * @param $url
-	 * @return JSON
+	 * Get the most recent media published by a user. To get the most recent media published by the owner of the access token, you can use self instead of the user-id.
+	 * @param $count - Count of media to return.
+	 * @param $minId - Return media later than this min_id.
+	 * @param $maxId - Return media earlier than this max_id.
+	 * @return Array
+	 */
+	public function getMedia($count = false, $minId = false, $maxId = false)
+	{
+		$url = "https://api.instagram.com/v1/users/{$this->userId}/media/recent/";
+		$url .= "?client_id=" . $this->clientId;
+		if ($count) $url .= "&count=" . $count;
+		if ($minId) $url .= "&min_id=" . $minId;
+		if ($maxId) $url .= "&max_id=" . $maxId;
+		return $this->cUrl($url);
+	}
+
+	/**
+	 * Search for a user by name.
+	 * @param $q - A query string.
+	 * @return Array
+	 */
+	public function getSearch($q)
+	{
+		$url = "https://api.instagram.com/v1/users/search";
+		$url .= "?q=" . $q;
+		$url .= "&client_id=" . $this->clientId;
+		return $this->cUrl($url);
+	}
+
+	/**
+	 * @param $url - A query url
+	 * @return Array
 	 */
 	private function cUrl($url)
 	{
 		$ch = curl_init($url); // Инициализируем сессию cURL
 		// Устанавливаем параметры cURL
-		//curl_setopt($ch, CURLOPT_URL, $url); // Url страницы
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); // Возвращает веб-страницу
 		curl_setopt($ch, CURLOPT_TIMEOUT, 20); // Таймаут ответа
 		$result = curl_exec($ch); // Выполняем запрос
 		curl_close($ch); // Завершаем сессию cUrl
-		return $result;
+		return json_decode($result);
 	}
 }
